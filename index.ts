8d804d74cad577a920d0ba6523ef9032f8cbe024
GUHADA <jaejin.yoon@temco.io>
2023-08-03 18:39:11 +0900

    langChain study

--- index.ts
+++ index.ts
@@ -20,6 +20,14 @@ import { subscribingHadler } from "Basic/1.Model_IO/2.languageModels/1.LLMs/1.Ho
 import { abortHandler } from "Basic/1.Model_IO/2.languageModels/2.chatModels/How-To/1.CancellingRequest";
 import { chatChain } from "Basic/1.Model_IO/2.languageModels/2.chatModels/How-To/2.LLMChainAnd3.Prompts";
 import { chatStreaming } from "Basic/1.Model_IO/2.languageModels/2.chatModels/How-To/4.Streaming";
+import {
+  parsingJsonDataHandler,
+  zodDataHandler,
+} from "Basic/1.Model_IO/3.outPutParsers/1.outPutParsers";
+import { zodDataWithLLMChainsHandler } from "Basic/1.Model_IO/3.outPutParsers/1.How-To/1.UseWithLLMChains";
+import { getCombingOutPutParseData } from "Basic/1.Model_IO/3.outPutParsers/2.CombiningOutputParsers";
+import { getListParser } from "Basic/1.Model_IO/3.outPutParsers/3.ListParser";
+import { getCutomListParser } from "Basic/1.Model_IO/3.outPutParsers/4.CustomListParser";
 
 // prompt templates
 // createAPromptTemplateHandler();
@@ -44,5 +52,11 @@ import { chatStreaming } from "Basic/1.Model_IO/2.languageModels/2.chatModels/Ho
 // console.log(streaming);
 // abortHandler();
 // console.log(chatChain);
-console.log(chatStreaming);
+// console.log(chatStreaming);
+// parsingJsonDataHandler();
+// zodDataHandler();
+// zodDataWithLLMChainsHandler();
+// console.log(getCombingOutPutParseData);
+// getListParser();
+console.log(getCutomListParser);
 export {};
